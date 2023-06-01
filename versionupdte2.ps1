9aac56351633b541a45d5d4de6f0440534fa1945
Suryansh2905 <134330084+Suryansh2905@users.noreply.github.com>
2023-06-01 16:19:09 -0400

    Update versionupdte2.ps1
    added description

--- versionupdte2.ps1
+++ versionupdte2.ps1
@@ -1,3 +1,33 @@
+<#
+.SYNOPSIS
+This PowerShell script updates a text file with a new version number in a local Git repository, commits the changes, and pushes them back to GitHub.
+
+.DESCRIPTION
+The script takes three parameters:
+- RepoPath: The path to the local Git repository directory.
+- FilePath: The path to the specific file within the repository to be updated.
+- NewVersion: The new version number to append to the file.
+
+The script changes the current working directory to the specified RepoPath. It then appends the NewVersion to the file specified by FilePath using the Add-Content cmdlet. The file is staged for commit using the git add command. The changes are committed with a commit message using the git commit command. Finally, the changes are pushed to the remote repository's main branch using the git push command with the origin remote.
+
+.PARAMETER RepoPath
+The path to the local Git repository directory.
+
+.PARAMETER FilePath
+The path to the specific file within the repository to be updated.
+
+.PARAMETER NewVersion
+The new version number to append to the file.
+#>
+param (
+    [string]$RepoPath,
+    [string]$FilePath,
+    [string]$NewVersion
+)
+
+# Rest of the script...
+
+
 param (
     [string]$RepoPath,
     [string]$FilePath,
