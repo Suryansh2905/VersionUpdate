16b0c9e2efff425dd350cd1ea40120a43054dacd
Suryansh2905 <134330084+Suryansh2905@users.noreply.github.com>
2023-06-01 14:56:15 -0400

    Update versionupdte2.ps1

--- versionupdte2.ps1
+++ versionupdte2.ps1
@@ -1,37 +1,29 @@
-﻿# Function to recursively locate all "version.txt" files under the given path
-function Get-VersionFiles($path) {
-    Get-ChildItem -Path $path -Filter "version.txt" -File -Recurse
-}
+param (
+    [string]$RootPath,
+    [string]$VersionNumber,
+    [string]$BranchName,
+    [string]$RemoteName
+)
 
-# Function to append version number to a file
-function Append-Version($filePath, $versionNumber) {
-    $content = Get-Content -Path $filePath
-    $content += "Version: $versionNumber"
-    $content | Set-Content -Path $filePath
-}
+# Change to the root directory
+Set-Location $RootPath
 
-# Input the path and version number
-$rootPath = Read-Host "Enter the root path:"
-$versionNumber = Read-Host "Enter the version number:"
+# Find all version.txt files recursively
+$versionFiles = Get-ChildItem -Path $RootPath -Filter "version.txt" -File -Recurse
 
-# Locate all "version.txt" files under the root path
-$versionFiles = Get-VersionFiles -path $rootPath
-
-# Append the version number to each file
+# Iterate over each version.txt file
 foreach ($file in $versionFiles) {
-    Append-Version -filePath $file.FullName -versionNumber $versionNumber
+    # Append the version number to the file
+    $fileContent = Get-Content $file.FullName
+    $fileContent += $VersionNumber
+    $fileContent | Set-Content $file.FullName
+
+    # Stage the file for commit
+    git add $file.FullName
 }
-
-# Commit and push the changes to GitHub
-$commitMessage = "Update version numbers"
-$branchName = Read-Host "Enter the branch name:"
-$remoteName = Read-Host "Enter the remote name (e.g., origin):"
-
-# Stage the modified files
-$versionFiles | ForEach-Object { git add $_.FullName }
 
 # Commit the changes
-git commit -m $commitMessage
+git commit -m "Update version numbers"
 
 # Push the changes to the remote repository
-git push $remoteName $branchName
+git push $RemoteName $BranchName
