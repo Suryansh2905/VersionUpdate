9b067550f1ddb008bfe15297d5df0cc53a6c8698
Suryansh2905 <134330084+Suryansh2905@users.noreply.github.com>
2023-06-05 14:06:01 -0400

    Update versionupdte2.ps1

--- versionupdte2.ps1
+++ versionupdte2.ps1
@@ -1,16 +1,25 @@
 param (
     [string]$RepoPath,
     [string]$FilePath,
-    [string]$NewVersion
+    [string]$NewVersion,
+    [string]$Branch = $null
 )
+
+if ($Branch -eq $null) {
+    $Branch = "main"
+}
 
 # Change to the repository directory
 Set-Location $RepoPath
 
+# Update the text file with the new version number
 Add-Content -Path $FilePath -Value $NewVersion
 
+# Stage the file for commit
 git add $FilePath
 
-git commit -m "Appended version number"
+# Commit the changes
+git commit -m "Update version number"
 
-git push origin main
+# Push the changes to the remote repository
+git push origin $Branch
