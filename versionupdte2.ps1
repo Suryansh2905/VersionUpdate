f6b7f51f2e577e4b6d7cbe3761da2a189fc648f1
Suryansh2905 <134330084+Suryansh2905@users.noreply.github.com>
2023-06-05 14:19:09 -0400

    Update versionupdte2.ps1

--- versionupdte2.ps1
+++ versionupdte2.ps1
@@ -1,3 +1,34 @@
+<#
+.SYNOPSIS
+Appends the released version number to the file, stages it for commit, and pushes the changes to a specified branch in a Git repository.
+
+.DESCRIPTION
+Appends the released version number to the file, stages it for commit, and pushes the changes to a specified branch in a Git repository.
+
+.PARAMETER RepoPath
+The path to the repository directory.
+
+.PARAMETER FilePath
+The path to the file to which the released version number needs to be appended.
+
+.PARAMETER NewVersion
+The new version number to be added to the file.
+
+.PARAMETER Branch
+(Optional) The name of the branch to push the changes to. If not specified, it defaults to "main".
+
+.EXAMPLE
+.\Update-Version.ps1 -RepoPath "C:\path\to\repository" -FilePath "version.txt" -NewVersion "1.2.3"
+
+This example updates the "version.txt" file in the specified repository directory with the new version number "1.2.3". It pushes the changes to the "main" branch in the remote repository.
+
+.EXAMPLE
+.\Update-Version.ps1 -RepoPath "C:\path\to\repository" -FilePath "version.txt" -NewVersion "1.2.3" -Branch "develop"
+
+This example updates the "version.txt" file in the specified repository directory with the new version number "1.2.3". It pushes the changes to the "develop" branch in the remote repository.
+
+#>
+
 param (
     [string]$RepoPath,
     [string]$FilePath,
