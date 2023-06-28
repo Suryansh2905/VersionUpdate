85f41ae2944cdc19d1b43be4797d382a99f3b4ce
Suryansh Singh <134330084+Suryansh2905@users.noreply.github.com>
2023-06-28 13:36:33 -0400

    Update versionupdte2.ps1

--- versionupdte2.ps1
+++ versionupdte2.ps1
@@ -1,27 +1,25 @@
-Function Add-VersionEntry {
-    [CmdletBinding( SupportsShouldProcess )]
-    param (
-        [Parameter( Mandatory )]
-        [string]
-        $FilePath,
-        [version]
-        $NewVersion,
-        [string]
-        $Branch = 'main'
-    )
+[CmdletBinding( SupportsShouldProcess )]
+param (
+    [Parameter( Mandatory )]
+    [string]
+    $FilePath,
+    [version]
+    $NewVersion,
+    [string]
+    $Branch = 'main'
+)
 
-    if (-not (Test-Path $FilePath -PathType Leaf)) {
-        throw "Invalid file path: $FilePath"
-    }
-    if ( $PSCmdlet.ShouldProcess('Program adds the released version number to the specified file, commits the changes and pushes them.') ) {
-        Add-Content -Path $FilePath -Value $NewVersion
+if (-not (Test-Path $FilePath -PathType Leaf)) {
+    throw "Invalid file path: $FilePath"
+}
+if ( $PSCmdlet.ShouldProcess('Program adds the released version number to the specified file, commits the changes and pushes them.') ) {
+    Add-Content -Path $FilePath -Value $NewVersion
 
-        git add $FilePath
+    git add $FilePath
 
-        git checkout $Branch
+    git checkout $Branch
 
-        git commit -m 'Appended version number'
+    git commit -m 'Appended version number'
 
-        git push origin $Branch
-    }
+    git push origin $Branch
 }
